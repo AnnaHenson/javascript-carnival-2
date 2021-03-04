3aab7b18d812f96d00965947b9d798cd8d040bfe
Anna Henson <anna.henson@outlook.co.nz>
2021-03-05 12:00:43 +1300

    Updated unicorn html

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -4,21 +4,61 @@
 
 console.log("Inflate The Unicorn!")
 document.getElementById("unicorn1").onclick = pictureClick;
+document.getElementById("unicorn2").onclick = pictureClick;
+document.getElementById("unicorn3").onclick = pictureClick;
+
 
 function  pictureClick(event){
-  if (unicorn1Counter >= 3)
+  /*if (unicorn1Counter >= 3)
   {
     alert("Unicorn x says thank you")
     return
+  }*/
+  var target = event.target;
+
+  if(target.id == "unicorn1"){
+    if (unicorn1Counter >= 3)
+    {
+      alert("Unicorn 1 says thank you")
+    
+    }
+    unicorn1Counter++
+
+    if(unicorn1Counter == 4)
+       unicorn1Counter = 0
+
+    target.src = unicorn[unicorn1Counter]
   }
-  unicorn1Counter++;
-  document.getElementById("unicorn1").src = unicorn[unicorn1Counter]
-  unicorn2Counter++;
-  document.getElementById("unicorn2").src = unicorn[unicorn2Counter]
-  unicorn3Counter++;
-  document.getElementById("unicorn3").src = unicorn[unicorn3Counter]
-       
-  console.log("clicked")
+
+  if(target.id == "unicorn2") {
+      if (unicorn2Counter >=3)
+      {
+        alert("unicorn 2 says thankyou")
+
+      }unicorn2Counter++
+
+    if(unicorn2Counter == 4)
+       unicorn2Counter = 0
+
+    target.src = unicorn[unicorn2Counter]
+
+  }
+
+  if(target.id == "unicorn3") {
+    if(unicorn3Counter >=3)
+    {
+      alert("unicorn 3 says thankyou")
+    }
+     unicorn3Counter++
+
+    if(unicorn3Counter == 4)
+        unicorn3Counter = 0
+    
+
+    target.src = unicorn[unicorn3Counter]
+
+  }
+
 
 }
 var unicorn =[]
@@ -26,6 +66,9 @@ unicorn[0] = "./images/unicorn-0.png";
 unicorn[1] = "./images/unicorn-1.png";
 unicorn[2] = "./images/unicorn-2.png";
 unicorn[3] = "./images/unicorn-3.png";
+
+var inflation = [0,0,0]
+
 
 var unicorn1Counter=0
 
--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,4 +2,34 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
+// Randomly selecdft a cell//
+// make a mole appear//
+// Click on a mole//
+// Mole appears in another cell//
+// Play the audio for the mole which appears//
+
+var cells = document.getElementsByTagName ("TD");
+
+var randomIndex = Math.floor (Math .random() * cells.length); 
+
+var randomCell = cells [randomIndex] 
+
+var mole = document.createElement("img"); 
+
+mole.src = "./mole.png" ;
+mole.id = "mole"
+
+randomCell.appendChild(mole);
+
+mole.onClick = hitMole
+
+function hitMole() {
+  randomIndex = Math.floor (Math .random() * cells.length);
+  randomCell = cells.[randomIndex]
+  randomCell.appendChild(mole);
+
+  var audio = new Audio ('./whack-audio.wav');
+  audio.play();
+}
+
 console.log("Whack-a-Mole!")
